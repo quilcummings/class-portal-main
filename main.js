b33506df9bbb8649c2e12c4d9323cc2647f6cbe7
quilcummings <quil735@gmail.com>
2022-09-12 21:06:20 -0600

    added html, js, and css files

---
+++ main.js
@@ -0,0 +1,26 @@
+// javascript
+
+// deployment 
+
+// option 1: directly to a server
+// use either ssh or ftp to connect to a server as creative.colorado.edu if you have access
+// copy and paste files into the server's public_html directory
+
+// option 2: github pages
+// navigate to your github repositroy in web browser
+// click on the settings tab
+// click on pages
+// click on main branch
+// deploy from a branch
+// githubusername.github.io/reponame
+// find it in evironments on repo page
+
+// option 3: Vercel
+// go to vercel.com and create an account (or log in)
+// create new deployment
+// select github as git provider and log into your github account
+// choose repo from list
+// choose a root dir (if you want to deploy multiple pages from one repo)
+// DEPLOY
+
+
